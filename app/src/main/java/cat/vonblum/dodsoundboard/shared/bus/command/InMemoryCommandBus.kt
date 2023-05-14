8833798e57d4aebbe3aabdc5c4f0a073ea9ea377
Ricard <ricardbarnes@gmail.com>
2023-05-14 22:50:43 +0200

    refactor(app): remove unnecessary app bus exceptions

--- app/src/main/java/cat/vonblum/dodsoundboard/shared/bus/command/InMemoryCommandBus.kt
+++ app/src/main/java/cat/vonblum/dodsoundboard/shared/bus/command/InMemoryCommandBus.kt
@@ -4,18 +4,13 @@ import cat.vonblum.dodsoundboard.shared.domain.bus.command.Command
 import cat.vonblum.dodsoundboard.shared.domain.bus.command.CommandBus
 import cat.vonblum.dodsoundboard.shared.domain.bus.command.CommandHandler
 import cat.vonblum.shared.infrastructure.bus.MessageBus
-import cat.vonblum.shared.infrastructure.bus.exception.UnregisteredHandlerException
 
 class InMemoryCommandBus(commandHandlers: List<CommandHandler>) : CommandBus {
 
     private val messageBus = MessageBus(commandHandlers)
 
     override fun dispatch(command: Command) {
-        try {
-            messageBus.dispatch(command)
-        } catch (unregisteredHandlerException: UnregisteredHandlerException) {
-            throw UnregisteredCommandException.becauseOf(command)
-        }
+        messageBus.dispatch(command)
     }
 
 }
--- app/src/main/java/cat/vonblum/dodsoundboard/shared/bus/query/InMemoryQueryBus.kt
+++ app/src/main/java/cat/vonblum/dodsoundboard/shared/bus/query/InMemoryQueryBus.kt
@@ -5,22 +5,13 @@ import cat.vonblum.dodsoundboard.shared.domain.bus.query.QueryBus
 import cat.vonblum.dodsoundboard.shared.domain.bus.query.QueryHandler
 import cat.vonblum.dodsoundboard.shared.domain.bus.query.Response
 import cat.vonblum.shared.infrastructure.bus.MessageBus
-import cat.vonblum.shared.infrastructure.bus.exception.UnregisteredHandlerException
 
 class InMemoryQueryBus(queryHandlers: List<QueryHandler>) : QueryBus {
 
     private val messageBus = MessageBus(queryHandlers)
 
     override fun ask(query: Query): Response? {
-        val response: Any?
-
-        try {
-            response = messageBus.dispatch(query)
-        } catch (unregisteredHandlerException: UnregisteredHandlerException) {
-            throw UnregisteredQueryException.becauseOf(query)
-        }
-
-        return response as Response?
+        return messageBus.dispatch(query) as Response?
     }
 
 }
